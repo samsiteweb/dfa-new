4d1ea6c4518a1e32c41e212bb00e324ef0e641b1
samuel <samthedonz@gmail.com>
2023-09-04 23:43:33 +0100

    latest

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import AboutBanner from '../components/about/about-banner/about-banner';
 import AboutFounder from '../components/about/about-founder/about-founder';
-import AboutPractice from '../components/about/about-practice/about-practice';
-import Article from '../components/article/article';
-import Pricing from '../components/pricing/pricing';
+// import AboutPractice from '../components/about/about-practice/about-practice';
+// import Article from '../components/article/article';
+// import Pricing from '../components/pricing/pricing';
 import ContactForm from '../components/contact-form/contact-form';
 
 const About = () => {
